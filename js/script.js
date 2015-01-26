a4f5987ba7987abf5752fb16dece6af0a58a2917
hrxfab <hrxfab@gmail.com>
2015-01-26 17:57:16 +0100

    Master

---
+++ js/script.js
@@ -0,0 +1,16 @@
+/* 
+ * UOL - Programming the Internet - Group Project
+ * Week 3 - Assignment
+ * Fabien Huraux
+ * Joao Paulo Henriques Remedio  
+ * Kevin Gargo 
+ */
+
+/* 
+Hide images other than the first (0) one.
+ */
+
+function ParentMenu() {
+		document.documentElement.parentElement.setAttribute("class", "bmenu li a:hover");
+		}
+
